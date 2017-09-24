24d989a23fc29df2b67b25f3479fd19b88bb4463
Arnór Ragnarsson <arnor.ragnarsson13@gmail.com>
2017-09-24 16:18:52 +0000

    setja upp webpack, babel og postcss

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './src/containers/App';
+
+import './scss/styles.scss';
+
+render(
+  <App />, document.getElementById('app')
+)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './index.js',
+
+  output: {
+    path: __dirname + '/build',
+    filename: 'bundle.js',
+    publicPath: '/build/'
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot-loader', 'babel-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ["style-loader", "css-loader", "postcss-loader", "sass-loader"]
+      }
+    ]
+  }
+};
